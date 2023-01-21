2eed55f6f9db6db7517357555006f8e7ef90eceb
maemae22 <maemae2@naver.com>
2023-01-22 01:27:04 +0900

    #1 - Spring Boot 프로젝트 생성
    - 스프링 부트 프로젝트로 루트 디렉토리를 초기화하고, 개발 바탕을 만든다.
    - spring initalizr 로 디렉토리 초기화하기

---
+++ src/test/java/com/teamb/travel/TeamBBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamb.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamBBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
