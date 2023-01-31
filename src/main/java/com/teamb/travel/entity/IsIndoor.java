e8da194ff247fc62b57933059d65b2199e143f5e
DongHyos <dong87070@naver.com>
2023-01-31 13:51:25 +0900

    #28 - Entity 수정(Lombok 추가)
    각 Entity에 접근을 위한 @Getter와 기본적으로 필요한 생성자들을 추가해줬다. 단, 무분별한 수정을 막기 위해 @Setter는 제외

--- src/main/java/com/teamb/travel/entity/IsIndoor.java
+++ src/main/java/com/teamb/travel/entity/IsIndoor.java
@@ -1,15 +1,20 @@
 package com.teamb.travel.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class IsIndoor {
     @Id
     private Long placeId;
-
     private String mapX;
     private String mapY;
     private String placeName;
--- src/main/java/com/teamb/travel/entity/Place.java
+++ src/main/java/com/teamb/travel/entity/Place.java
@@ -1,16 +1,16 @@
 package com.teamb.travel.entity;
 
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.math.BigDecimal;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Place {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
