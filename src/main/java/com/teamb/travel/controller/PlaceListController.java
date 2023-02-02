2b87bc2018f1fead0a55b603b971abb7b7b5d560
maemae22 <maemae2@naver.com>
2023-02-02 16:01:58 +0900

    #38 - 관광지 목록 출력하는 Controller를 하나의 Controller로 합침
    - 기존 TourListController 삭제
    - PlaceListController로 합침 (관광지 목록 관련해서는 여기서 관리)

--- src/main/java/com/teamb/travel/controller/PlaceListController.java
+++ src/main/java/com/teamb/travel/controller/PlaceListController.java
@@ -4,6 +4,9 @@ import com.teamb.travel.dto.PlaceListReqDTO;
 import com.teamb.travel.dto.PlaceListResDTO;
 import com.teamb.travel.service.PlaceListService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +20,21 @@ public class PlaceListController {
     PlaceListService placeListService;
 
     @GetMapping("/tourlist/keyword")
-    public List<PlaceListResDTO> keywordPlacesSearch(@RequestParam String keyword, @RequestParam(name = "pageno") int pageNo) {
+    public List<PlaceListResDTO> keywordPlacesSearch(@RequestParam String keyword, Pageable pageable) {
 
         PlaceListReqDTO placeListReqDTO = new PlaceListReqDTO(keyword);
-        return placeListService.selectPlacesByTitleContaining(placeListReqDTO, pageNo);
+        return placeListService.selectPlacesByTitleContaining(placeListReqDTO, pageable);
+    }
+
+    @GetMapping("/tourlist/location")
+    public ResponseEntity<List<PlaceListResDTO>> tourListByLocation(Pageable pageable, @RequestParam("areacode") String areaCode) {
+        List<PlaceListResDTO> resDtos = placeListService.selectByLocationPlaceList(areaCode, pageable);
+        return new ResponseEntity<>(resDtos, HttpStatus.OK);
+    }
+
+    @GetMapping("/tourlist/hashtag")
+    public ResponseEntity<List<PlaceListResDTO>> tourListByLocation(Pageable pageable, @RequestParam("hashtag") List<String> hashtag) {
+        List<PlaceListResDTO> resDtos = placeListService.selectByTagPlaceList(hashtag, pageable);
+        return new ResponseEntity<>(resDtos, HttpStatus.OK);
     }
 }
