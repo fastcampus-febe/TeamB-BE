856be8f7bfffc9634f68d35570d6687abd277909
maemae22 <maemae2@naver.com>
2023-02-02 17:11:52 +0900

    #21 - PlaceRepository 필드명을 repository에서 placeRepository로 변경
    - 명확한 구분을 위해 repository에서 placeRepository으로 변경

--- src/main/java/com/teamb/travel/service/PlaceListService.java
+++ src/main/java/com/teamb/travel/service/PlaceListService.java
@@ -5,32 +5,29 @@ import com.teamb.travel.dto.PlaceListResDTO;
 import com.teamb.travel.entity.IsIndoor;
 import com.teamb.travel.entity.Place;
 import com.teamb.travel.entity.PlaceDetail;
-import com.teamb.travel.entity.Reply;
 import com.teamb.travel.repository.IsIndoorRepository;
 import com.teamb.travel.repository.PlaceDetailRepository;
 import com.teamb.travel.repository.PlaceRepository;
 import com.teamb.travel.repository.ReplyRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PlaceListService {
 
-    private final PlaceRepository repository;
+    private final PlaceRepository placeRepository;
     private final PlaceDetailRepository placeDetailRepository;
     private final ReplyRepository replyRepository;
     private final IsIndoorRepository indoorRepository;
 
     public void saveEntity(Place place) {
-        repository.save(place);
+        placeRepository.save(place);
     }
 
     // placeList를 매개변수로 받으면 거기에 맞는 관광지 List를 반환하는 메서드
@@ -73,19 +70,19 @@ public class PlaceListService {
 
     public List<PlaceListResDTO> selectPlacesByTitleContaining(PlaceListReqDTO placeListReqDTO, Pageable pageable) {
 
-        List<Place> places = repository.findPlacesByTitleContaining(placeListReqDTO.getTitle(), pageable);
+        List<Place> places = placeRepository.findPlacesByTitleContaining(placeListReqDTO.getTitle(), pageable);
         return PlacesListByPlaceList(places);
     }
 
     public List<PlaceListResDTO> selectByLocationPlaceList(String areaCode, Pageable pageable) {
 
-        List<Place> places = repository.findAllByAreacodeOrderByIdAsc(areaCode, pageable);
+        List<Place> places = placeRepository.findAllByAreacodeOrderByIdAsc(areaCode, pageable);
         return PlacesListByPlaceList(places);
     }
 
     public List<PlaceListResDTO> selectByTagPlaceList(List<String> hashtag, Pageable pageable) {
 
-        List<Place> places = repository.findAllByCat3InOrderByIdAsc(hashtag, pageable);
+        List<Place> places = placeRepository.findAllByCat3InOrderByIdAsc(hashtag, pageable);
         return PlacesListByPlaceList(places);
     }
 
