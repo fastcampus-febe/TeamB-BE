2e48005b544af38f7f672c27cc2b2482b49b7c9f
maemae22 <maemae2@naver.com>
2023-02-05 16:16:08 +0900

    #47 - PlaceApi 주석 해제 및 Place에 생성자 생성
    - PlaceApi 주석 해제
    - Place(Entity)에 필요한 생성자가 없어서, 생성자 하나 만듦.
    - PlaceApi 추후 리팩토링 예정

--- src/main/java/com/teamb/travel/entity/Place.java
+++ src/main/java/com/teamb/travel/entity/Place.java
@@ -35,4 +35,33 @@ public class Place {
     private String tel;
     private String title;
     private String zipcode;
+
+    public Place(String addr1, String addr2, String areacode, String booktour,
+                 String cat1, String cat2, String cat3, String contentid,
+                 String contenttypeid, String createdtime, String firstimage,
+                 String firstimage2, String mapx, String mapy, String mlevel,
+                 String modifiedtime, String readcount, String sigungucode,
+                 String tel, String title, String zipcode) {
+        this.addr1 = addr1;
+        this.addr2 = addr2;
+        this.areacode = areacode;
+        this.booktour = booktour;
+        this.cat1 = cat1;
+        this.cat2 = cat2;
+        this.cat3 = cat3;
+        this.contentid = contentid;
+        this.contenttypeid = contenttypeid;
+        this.createdtime = createdtime;
+        this.firstimage = firstimage;
+        this.firstimage2 = firstimage2;
+        this.mapx = mapx;
+        this.mapy = mapy;
+        this.mlevel = mlevel;
+        this.modifiedtime = modifiedtime;
+        this.readcount = readcount;
+        this.sigungucode = sigungucode;
+        this.tel = tel;
+        this.title = title;
+        this.zipcode = zipcode;
+    }
 }
