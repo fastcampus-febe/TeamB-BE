d7085c56c66b45351af195694a84ca71c255e79c
maemae22 <maemae2@naver.com>
2023-02-06 18:17:59 +0900

    #69 - api 호출하는 부분과 DB에 저장하는 부분을 분리 (PlaceDetailApi)
    - PlaceDetailApi : Open API를 직접 호출하여 정보를 JSONArray로 받아옴
    
    - PlaceDetailApiService : api에서 받아온 JSON data를 DB에 저장
    - 예외 처리함 : IOException, ParseException 발생 가능
    - NullPointerException : Open api 호출 가능 최대 횟수 초과 시 받아오는 정보가 없어 발생

--- src/main/java/com/teamb/travel/api/PlaceDetailApi.java
+++ src/main/java/com/teamb/travel/api/PlaceDetailApi.java
@@ -1,97 +1,54 @@
 package com.teamb.travel.api;
 
-import com.teamb.travel.entity.PlaceDetail;
-import com.teamb.travel.repository.PlaceDetailRepository;
-import com.teamb.travel.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.net.URLEncoder;
-import java.text.ParseException;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class PlaceDetailApi {
+
     @Value("${api.serviceKey}")
     private String serviceKey;
 
-    private final PlaceDetailRepository placeDetailRepository;
-    private final PlaceRepository placeRepository;
+    // 관광지 상세정보 제공하는 외부 api에서 관광지 상세정보를 가져와 JSONArray로 반환함
+    // 매개변수로 받은 contentId에 해당하는 관광지의 상세정보 반환
+
     private final UrlBuilderToJSONArray urlBuilderToJSONArray;
 
-    int count = 9915;
-    private int NUMBER = count;
+    public JSONArray placeDetailCommonJSONArrayByContentId(String contentId) throws IOException, ParseException, NullPointerException {
 
-//    @PostConstruct
-    public void tourDetailTestCheck() throws IOException, ParseException, org.json.simple.parser.ParseException {
-        List<String> contentIds = placeRepository.findByAllContentid();
-        System.out.println(contentIds.size());
-        try {
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551011/KorService/detailCommon"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("contentTypeId", "UTF-8") + "=" + URLEncoder.encode("12", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("contentId", "UTF-8") + "=" + URLEncoder.encode(contentId, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("MobileOS", "UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("MobileApp", "UTF-8") + "=" + URLEncoder.encode("AppTest", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("defaultYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("firstImageYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("areacodeYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("catcodeYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("addrinfoYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("mapinfoYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("overviewYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8"));
 
-            for (int i = NUMBER; i < contentIds.size(); i++) {
-                StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551011/KorService/detailCommon"); /*URL*/
-                NUMBER = i;
-                // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
-                urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey); /*Service Key*/
-                urlBuilder.append("&" + URLEncoder.encode("contentTypeId", "UTF-8") + "=" + URLEncoder.encode("12", "UTF-8")); /*지역*/
-                urlBuilder.append("&" + URLEncoder.encode("contentId", "UTF-8") + "=" + URLEncoder.encode(contentIds.get(i), "UTF-8")); /*지역*/
-                urlBuilder.append("&" + URLEncoder.encode("MobileOS", "UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8")); /*XML 또는 JSON*/
-                urlBuilder.append("&" + URLEncoder.encode("MobileApp", "UTF-8") + "=" + URLEncoder.encode("AppTest", "UTF-8")); /*날짜*/
-                urlBuilder.append("&" + URLEncoder.encode("defaultYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8")); /*지역*/
-                urlBuilder.append("&" + URLEncoder.encode("firstImageYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8")); /*지역*/
-                urlBuilder.append("&" + URLEncoder.encode("areacodeYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8")); /*지역*/
-                urlBuilder.append("&" + URLEncoder.encode("catcodeYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8")); /*지역*/
-                urlBuilder.append("&" + URLEncoder.encode("addrinfoYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8")); /*지역*/
-                urlBuilder.append("&" + URLEncoder.encode("mapinfoYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8")); /*지역*/
-                urlBuilder.append("&" + URLEncoder.encode("overviewYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8")); /*지역*/
+        return urlBuilderToJSONArray.urlBuilderToJSONArray(urlBuilder, DataType.XML);
+    }
 
+    public JSONArray placeDetailIntroJSONArrayByContentId(String contentId) throws IOException, ParseException, NullPointerException {
 
-                StringBuilder urlBuilder2 = new StringBuilder("http://apis.data.go.kr/B551011/KorService/detailIntro"); /*URL*/
-                // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
-                urlBuilder2.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + "51%2Bc6%2BNBpG7JFhyeZbKn4BDMVtvgE6W15oUad4G2n74%2Bv7Bo4oHKoQL%2FwLSaBnD67u%2F5CorapB5I6WMBLpXEkg%3D%3D"); /*Service Key*/
-                urlBuilder2.append("&" + URLEncoder.encode("contentTypeId", "UTF-8") + "=" + URLEncoder.encode("12", "UTF-8")); /*지역*/
-                urlBuilder2.append("&" + URLEncoder.encode("contentId", "UTF-8") + "=" + URLEncoder.encode(contentIds.get(i), "UTF-8")); /*지역*/
-                urlBuilder2.append("&" + URLEncoder.encode("MobileOS", "UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8")); /*XML 또는 JSON*/
-                urlBuilder2.append("&" + URLEncoder.encode("MobileApp", "UTF-8") + "=" + URLEncoder.encode("AppTest", "UTF-8")); /*날짜*/
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551011/KorService/detailIntro"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("contentTypeId", "UTF-8") + "=" + URLEncoder.encode("12", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("contentId", "UTF-8") + "=" + URLEncoder.encode(contentId, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("MobileOS", "UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("MobileApp", "UTF-8") + "=" + URLEncoder.encode("AppTest", "UTF-8"));
 
-
-                JSONObject parse_item = (JSONObject) urlBuilderToJSONArray.urlBuilderToJSONArray(urlBuilder, DataType.XML).get(0);
-//                org.json.simple.JSONObject parse_item = (org.json.simple.JSONObject) parse_items.get("item"); // items로 부터 itemlist 를 받기
-
-                JSONObject parse_item2 = (JSONObject) urlBuilderToJSONArray.urlBuilderToJSONArray(urlBuilder2, DataType.XML).get(0);
-//                org.json.simple.JSONObject parse_item2 = (org.json.simple.JSONObject) parse_items2.get("item"); // items로 부터 itemlist 를 받기
-
-                PlaceDetail placeDetail = new PlaceDetail(String.valueOf(parse_item.get("contentid")),
-                        String.valueOf(parse_item.get("title")),
-                        String.valueOf(parse_item.get("telname")),
-                        String.valueOf(parse_item.get("homepage")),
-                        String.valueOf(parse_item.get("overview")),
-                        String.valueOf(parse_item2.get("parking")),
-                        String.valueOf(parse_item2.get("expagerange")),
-                        String.valueOf(parse_item2.get("chkpet")),
-                        String.valueOf(parse_item2.get("infocenter")),
-                        String.valueOf(parse_item2.get("restdate")),
-                        String.valueOf(parse_item2.get("opendate")),
-                        String.valueOf(parse_item2.get("accomcount")),
-                        String.valueOf(parse_item2.get("usetime")),
-                        String.valueOf(parse_item2.get("heritage1")),
-                        String.valueOf(parse_item2.get("heritage2")),
-                        String.valueOf(parse_item2.get("heritage3")),
-                        String.valueOf(parse_item2.get("userseason")),
-                        String.valueOf(parse_item2.get("chkbabycarriage")),
-                        String.valueOf(parse_item2.get("expguide")),
-                        String.valueOf(parse_item2.get("chkcreditcard")));
-
-                placeDetailRepository.save(placeDetail);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(NUMBER);
-        }
+        return urlBuilderToJSONArray.urlBuilderToJSONArray(urlBuilder, DataType.XML);
     }
 }
